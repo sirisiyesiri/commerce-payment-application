c47e45c4a75e4ca0ca47a2cd28529bb1dfa288de
ChoiT815 <choi081517@gmail.com>
2026-06-02 18:05:56 +0900

    refactor : LoginResponse 수정

--- src/main/java/com/example/commercepaymentapplication/domain/auth/dto/LoginResponse.java
+++ src/main/java/com/example/commercepaymentapplication/domain/auth/dto/LoginResponse.java
@@ -1,24 +1,28 @@
 package com.example.commercepaymentapplication.domain.auth.dto;
 
 import com.example.commercepaymentapplication.domain.user.entity.User;
+import lombok.Getter;
 
+@Getter
 public class LoginResponse {
-
     private final Long userId;
     private final String email;
     private final String name;
+    private final String token;
 
-    public LoginResponse(Long userId, String email, String name) {
+    public LoginResponse(Long userId, String email, String name, String token) {
         this.userId = userId;
         this.email = email;
         this.name = name;
+        this.token = token;
     }
 
-    public static LoginResponse from(User user) {
+    public static LoginResponse of(User user, String token) {
         return new LoginResponse(
                 user.getId(),
                 user.getEmail(),
-                user.getName()
+                user.getName(),
+                token
         );
     }
 }
