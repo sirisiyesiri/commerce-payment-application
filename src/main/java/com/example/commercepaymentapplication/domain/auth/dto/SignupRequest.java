928a9c238f59423add7cd3f28e072686d31b0e6b
ChoiT815 <choi081517@gmail.com>
2026-06-02 17:59:12 +0900

    refactor : SignupRequest 수정

--- src/main/java/com/example/commercepaymentapplication/domain/auth/dto/SignupRequest.java
+++ src/main/java/com/example/commercepaymentapplication/domain/auth/dto/SignupRequest.java
@@ -7,17 +7,17 @@ import lombok.Getter;
 
 @Getter
 public class SignupRequest {
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수 입력값입니다.")
     String name;
 
-    @NotBlank
+    @NotBlank(message = "전화번호는 필수 입력값입니다.")
     String phoneNumber;
 }
