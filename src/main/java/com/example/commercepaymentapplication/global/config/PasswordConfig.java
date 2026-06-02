ae1e30e3b9228e5189701b5fd4ffa366a791f260
ChoiT815 <choi081517@gmail.com>
2026-06-02 20:46:01 +0900

    refactor : PasswordConfig 수정

--- src/main/java/com/example/commercepaymentapplication/global/config/PasswordConfig.java
+++ src/main/java/com/example/commercepaymentapplication/global/config/PasswordConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class PasswordConfig {
