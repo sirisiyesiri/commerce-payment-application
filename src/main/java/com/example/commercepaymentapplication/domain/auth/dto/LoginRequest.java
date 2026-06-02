2cb4599c4fbcc87e0a75c5b454df25ee78576111
ChoiT815 <choi081517@gmail.com>
2026-06-02 18:00:32 +0900

    refactor : LoginRequest 수정

--- src/main/java/com/example/commercepaymentapplication/domain/auth/dto/LoginRequest.java
+++ src/main/java/com/example/commercepaymentapplication/domain/auth/dto/LoginRequest.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 @Getter
 public class LoginRequest {
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 }
