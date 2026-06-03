5c90566e1dcd6f08773c30e34089abe7b8065914
HAN-DABON <sinmint11@gmail.com>
2026-06-03 16:49:02 +0900

    feat(cart): CartController, CartService 기본 세팅

--- src/main/java/com/example/commercepaymentapplication/domain/cart/controller/CartController.java
+++ src/main/java/com/example/commercepaymentapplication/domain/cart/controller/CartController.java
@@ -1,4 +1,14 @@
 package com.example.commercepaymentapplication.domain.cart.controller;
 
+import com.example.commercepaymentapplication.domain.cart.repository.CartItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/carts")
+@RestController
 public class CartController {
+
+    private final CartItemRepository cartItemRepository;
 }
--- src/main/java/com/example/commercepaymentapplication/domain/cart/service/CartService.java
+++ src/main/java/com/example/commercepaymentapplication/domain/cart/service/CartService.java
@@ -1,4 +1,16 @@
 package com.example.commercepaymentapplication.domain.cart.service;
 
+import com.example.commercepaymentapplication.domain.cart.repository.CartItemRepository;
+import com.example.commercepaymentapplication.domain.product.repository.ProductRepository;
+import com.example.commercepaymentapplication.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
+
+    private final CartItemRepository cartItemRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 }
