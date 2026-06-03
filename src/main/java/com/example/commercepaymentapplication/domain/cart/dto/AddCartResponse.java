395c692f174b18dd77e06434184451e2a4065fba
HAN-DABON <sinmint11@gmail.com>
2026-06-03 18:34:12 +0900

    feat(cart): dto 생성

---
+++ src/main/java/com/example/commercepaymentapplication/domain/cart/dto/AddCartResponse.java
@@ -0,0 +1,7 @@
+package com.example.commercepaymentapplication.domain.cart.dto;
+
+import com.example.commercepaymentapplication.domain.cart.entity.CartItem;
+
+public record AddCartResponse(
+        Long cartItemId)
+{}
