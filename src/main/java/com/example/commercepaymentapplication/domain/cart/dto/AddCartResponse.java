1c7a7d75c87374c78513688fec1ba69de704fdc4
HAN-DABON <sinmint11@gmail.com>
2026-06-03 19:28:25 +0900

    fix: 잘못된 import 삭제

--- src/main/java/com/example/commercepaymentapplication/domain/cart/dto/AddCartResponse.java
+++ src/main/java/com/example/commercepaymentapplication/domain/cart/dto/AddCartResponse.java
@@ -1,7 +1,5 @@
 package com.example.commercepaymentapplication.domain.cart.dto;
 
-import com.example.commercepaymentapplication.domain.cart.entity.CartItem;
-
 public record AddCartResponse(
         Long cartItemId)
 {}
