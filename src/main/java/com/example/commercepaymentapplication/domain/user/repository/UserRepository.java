ad58a59617303d5ffab525f5f0ab61f951fe8451
WookJaes <dnrwo09@gmail.com>
2026-06-05 14:04:45 +0900

    feat: UserRepository 포인트 변경 비관락 조회 추가

--- src/main/java/com/example/commercepaymentapplication/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/commercepaymentapplication/domain/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.commercepaymentapplication.domain.user.repository;
 
 import com.example.commercepaymentapplication.domain.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.cartItems WHERE u.id = :userId")
     Optional<User> findByIdWithCartItems(@Param("userId") Long userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u WHERE u.id = :userId")
+    Optional<User> findByIdForUpdate(@Param("userId") Long userId);
 }
