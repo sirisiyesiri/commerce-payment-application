d82508d5822a564cfb22efd529cd8047e14f7261
HAN-DABON <sinmint11@gmail.com>
2026-06-05 16:19:57 +0900

    feat(order): 주문 기준 결제 조회 기능 추가
    - 주문 ID 기준 결제 조회 Repository 메서드 추가
    - 주문 ID 기준 결제 엔티티 조회 Service 메서드 추가
    - 결제 정보가 없을 경우 PAYMENT_NOT_FOUND 예외 처리

--- src/main/java/com/example/commercepaymentapplication/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/commercepaymentapplication/domain/payment/repository/PaymentRepository.java
@@ -3,5 +3,10 @@ package com.example.commercepaymentapplication.domain.payment.repository;
 import com.example.commercepaymentapplication.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByOrder_Id(Long orderId);
+
 }
