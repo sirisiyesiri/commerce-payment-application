7c5cde8f3ac898a789219c77d36e7f6b3ef717ba
WookJaes <dnrwo09@gmail.com>
2026-06-09 15:25:53 +0900

    refactor: 정적 페이지 컨트롤러 위치 변경

--- src/main/java/com/example/commercepaymentapplication/global/controller/StaticPageController.java
+++ src/main/java/com/example/commercepaymentapplication/global/controller/StaticPageController.java
@@ -1,4 +1,4 @@
-package com.example.commercepaymentapplication.global.config;
+package com.example.commercepaymentapplication.global.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
