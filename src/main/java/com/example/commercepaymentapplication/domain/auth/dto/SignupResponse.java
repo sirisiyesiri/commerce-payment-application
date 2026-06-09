a441e7f8dd1c4d7c0da1bc9a1ffc083f15a0721f
sirisiyesiri <shil690321@naver.com>
2026-06-09 12:36:02 +0900

    refactor: Auth DTO class → recode로 수정

--- src/main/java/com/example/commercepaymentapplication/domain/auth/dto/SignupResponse.java
+++ src/main/java/com/example/commercepaymentapplication/domain/auth/dto/SignupResponse.java
@@ -1,20 +1,12 @@
 package com.example.commercepaymentapplication.domain.auth.dto;
 
 import com.example.commercepaymentapplication.domain.user.entity.User;
-import lombok.Getter;
 
-@Getter
-public class SignupResponse {
-
-    private final Long userId;
-    private final String email;
-    private final String name;
-
-    public SignupResponse(Long userId, String email, String name) {
-        this.userId = userId;
-        this.email = email;
-        this.name = name;
-    }
+public record SignupResponse(
+        Long userId,
+        String email,
+        String name
+) {
 
     public static SignupResponse from(User user) {
         return new SignupResponse(
--- src/main/java/com/example/commercepaymentapplication/domain/user/dto/GetUserResponse.java
+++ src/main/java/com/example/commercepaymentapplication/domain/user/dto/GetUserResponse.java
@@ -1,22 +1,13 @@
 package com.example.commercepaymentapplication.domain.user.dto;
 
 import com.example.commercepaymentapplication.domain.user.entity.User;
-import lombok.Getter;
 
-@Getter
-public class GetUserResponse {
-
-    private final Long userId;
-    private final String email;
-    private final String name;
-    private final String phoneNumber;
-
-    public GetUserResponse(Long userId, String email, String name, String phoneNumber) {
-        this.userId = userId;
-        this.email = email;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
+public record GetUserResponse(
+        Long userId,
+        String email,
+        String name,
+        String phoneNumber
+) {
 
     public static GetUserResponse from(User user) {
         return new GetUserResponse(
